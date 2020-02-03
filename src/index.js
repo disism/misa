2547819cb1c1493bd93238a66c123db1397eb485
disism <x@disism.com>
2020-02-03 15:09:18 +0800

    header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-console.log('hello webpack');
 import './index.scss'
+import './scss/header.scss'
+
+const searchInp = document.querySelector('.search-input')
+
+searchInp.addEventListener('click', function () {
+    searchInp.style.width = '10rem'
+})
+searchInp.addEventListener('blur', function () {
+    searchInp.style.width = '3rem'
+})
