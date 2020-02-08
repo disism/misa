f1ea10fbe34831c7fd9e60383c062a5b15121059
disism <x@disism.com>
2020-02-08 11:01:25 +0800

    fix bug

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,9 @@ module.exports = {
     plugins: [
       require('autoprefixer')({
         "browsers": [
-            "defaults",
-            "not ie < 11",
+            "> 5%",
             "last 2 versions",
-            "> 1%",
-            "iOS 7",
-            "last 3 iOS versions"
+            "@-webkit-keyframes"
         ]
       })
     ]
